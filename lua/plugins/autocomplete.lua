189baf262d2b6f267cbece4a50459f43f281332d
Trenton <trenflem@gmail.com>
2023-03-18 12:48:36 -0400

    chunk up plugins for fun

---
+++ lua/plugins/autocomplete.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    'tpope/vim-sleuth',
+
+    { -- Autocompletion
+      'hrsh7th/nvim-cmp',
+      dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+    },
+
+    -- Useful plugin to show you pending keybinds.
+    { 'folke/which-key.nvim', opts = {} },
+
+
+
+    { -- Add indentation guides even on blank lines
+      'lukas-reineke/indent-blankline.nvim',
+      -- Enable `lukas-reineke/indent-blankline.nvim`
+      -- See `:help indent_blankline.txt`
+      opts = {
+        char = '┊',
+        show_trailing_blankline_indent = false,
+      },
+    },
+
+  }
+}
