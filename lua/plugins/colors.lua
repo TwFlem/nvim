92fece35b0d35210b40ce00d3a6e40378397957a
Trenton Fleming <trenton.fleming1@accesscfa.com>
2023-04-01 01:06:59 -0400

    more contrast

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -3,7 +3,7 @@ return {
     'folke/tokyonight.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'tokyonight-moon'
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 }
