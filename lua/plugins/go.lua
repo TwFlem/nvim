d6ac57e60a756847cb40aa56fabf33510721eb3b
Trenton Fleming <trenton.fleming1@accesscfa.com>
2023-04-14 13:35:04 -0400

    better go mapping for interfaces

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -4,7 +4,8 @@ local init = function ()
  vim.keymap.set('n', '<leader>got', '<cmd>GoTestFunc<cr>', { desc = '[G][O] [T]est Func'})
  vim.keymap.set('n', '<leader>goT', '<cmd>GoTest<cr>', { desc = '[G][O] run all [T]ests'})
  vim.keymap.set('n', '<leader>god', '<cmd>GoDoc<cr>', { desc = '[G][O] [D]oc'})
- vim.keymap.set('n', '<leader>goi', '<cmd>GoInfo<cr>', { desc = '[G][O] [I]nfo'})
+ vim.keymap.set('n', '<leader>goi', '<cmd>GoImplements<cr>', { desc = '[G][O] [I]mplements - see what implements the interface or method under cursor'})
+ vim.keymap.set('n', '<leader>goI', '<cmd>GoImplements<cr>', { desc = '[G][O] [I]mplement - generate stub for interface under cursor'})
 end
 return {
   {
