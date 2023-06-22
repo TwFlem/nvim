a8343370b5e6bcf740d3a92ca2b682eccf9b507f
Trenton Fleming <trenton.fleming1@accesscfa.com>
2023-06-22 11:10:51 -0400

    clear search

--- init.lua
+++ init.lua
@@ -104,6 +104,7 @@ vim.keymap.set('n', '<C-l>', '<C-W>l', { desc = 'Focus window right' })
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', '<C-W>t', '<cmd>tabnew<cr>', { desc = 'New Empty [T]ab' })
+vim.keymap.set('n', '<C-c>', '<cmd>noh<cr>', { desc = 'Clear Last Search' })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
